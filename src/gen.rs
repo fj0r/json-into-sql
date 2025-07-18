e5250a03cf74c975b42446184313dd53e1079c8e
fj0r <fj0rd@qq.com>
2025-07-18 17:06:57 +0800

    chore: update toml crate and refactor schema generation

--- src/gen.rs
+++ src/gen.rs
@@ -1,43 +1,144 @@
-use serde::Deserialize;
-use std::collections::HashMap;
 use anyhow::Result;
+use serde::Deserialize;
+//use toml::map::Map;
+use std::collections::BTreeMap as Map;
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
-enum Kind {
+enum Typ {
+    #[allow(non_camel_case_types)]
     #[serde(rename = "type")]
     literal(String),
-    references(Vec<String>),
+    #[allow(non_camel_case_types)]
+    references((String, String)),
+    #[allow(non_camel_case_types)]
     #[serde(rename = "enum")]
     variant(Vec<String>),
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct Field {
     #[serde(flatten, rename = "type")]
-    kind: Kind,
+    typ: Typ,
     default: Option<String>,
     #[serde(default)]
     uniq: bool,
     #[serde(default)]
-    notnull: bool
+    notnull: bool,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct Table {
-    fields: HashMap<String, Field>,
+    column: Map<String, Field>,
     primary: Option<Vec<String>>,
     index: Option<Vec<String>>,
 }
 
+struct Cols {
+    name: String,
+    typ: String,
+    x: Vec<String>,
+    refs: Option<String>,
+    enu: Option<Vec<String>>,
+}
 
-fn main() -> Result<()> {
-    let f = std::env::args().nth(1).and_then(|x| {
-        std::fs::read_to_string(x).ok()
-    });
-    if let Some(f) = f {
-        let x: HashMap<String, Table> = toml::from_str(&f).unwrap();
-        println!("{:#?}", &x);
+impl std::fmt::Display for Cols {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut x = self.x.clone();
+        if let Some(r) = &self.refs {
+            x.push(r.to_string())
+        }
+        write!(f, "    {} {} {}", self.name, self.typ, x.join(" "),)
+    }
+}
+
+fn gen_columns(
+    table: &String,
+    name: &String,
+    field: &Field,
+    schema: &Map<String, Table>,
+) -> Cols {
+    let mut enu = None;
+    let mut refs = None;
+    let mut x = Vec::new();
+    let typ = match &field.typ {
+        Typ::literal(l) => l.to_uppercase(),
+        Typ::references((table, column)) => {
+            let t = schema.get(table).unwrap();
+            let c = t.column.get(column).unwrap();
+            refs = Some(format!("REFERENCES {} ({})", table, column));
+            match &c.typ {
+                Typ::literal(x) => match x.as_str() {
+                    "serial" => "integer",
+                    "bigserial" => "biginteger",
+                    _ => x,
+                }
+                .to_uppercase(),
+                _ => unreachable!(),
+            }
+        }
+        Typ::variant(v) => {
+            enu = Some(v.clone());
+            format!("{}_{}", table, name)
+        }
+    };
+    if field.notnull {
+        x.push("NOT NULL".to_string());
+    }
+    if let Some(d) = &field.default {
+        let v = match typ.as_str() {
+            "JSONB" => format!("'{}'::JSONB", d),
+            _ => d.to_owned(),
+        };
+        x.push(format!("DEFAULT {}", v));
+    }
+    if field.uniq {
+        x.push("UNIQ".to_string());
     }
 
+    Cols {
+        name: name.to_string(),
+        typ,
+        x,
+        refs,
+        enu,
+    }
+}
+
+fn gen_table(schema: &Map<String, Table>) -> Vec<String> {
+    let mut r = Vec::new();
+    for (k, v) in schema.iter() {
+        let mut cs = Vec::new();
+        for (l, w) in v.column.iter() {
+            let x = gen_columns(k, l, w, schema);
+            if let Some(en) = &x.enu {
+                let m: Vec<_> = en.into_iter().map(|x| format!("'{x}'")).collect();
+                let m = m.join(", ");
+                r.push(format!("CREATE TYPE {} AS ENUM ({});", x.typ, m))
+            }
+            cs.push(x.to_string());
+        }
+        if let Some(p) = &v.primary {
+            cs.push(format!("    PRIMARY KEY ({})", p.join(", ")))
+        }
+        let cs = cs.join(",\n");
+        r.push(format!("CREATE TABLE {} (\n{}\n);", k, cs));
+    }
+    r
+}
+
+fn main() -> Result<()> {
+    let f = std::env::args()
+        .nth(1)
+        .and_then(|x| std::fs::read_to_string(x).ok());
+    if let Some(f) = f {
+        let x: Map<String, Table> = toml::from_str(&f).unwrap();
+        println!("{:#?}", &x);
+        for s in gen_table(&x) {
+            println!("{}", &s);
+        }
+    }
     Ok(())
 }
